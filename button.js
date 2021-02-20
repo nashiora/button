a9302d815b6e60bfd12a7b6b4c7bbbc5ff720ffb
Cayman Gill <cayman.g@hotmail.com>
2021-02-20 14:38:42 -0500

    Button
    test

---
+++ button.js
@@ -0,0 +1,9 @@
+function buttonPush()
+{
+    document.getElementById('button').setAttribute('src','assets/button-push.png')
+}
+
+function buttonUnpush()
+{
+    document.getElementById('button').setAttribute('src','assets/button.png')
+}
